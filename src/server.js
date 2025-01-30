7c5d5648b3aef710804bc281f4e40027e4e641d2
Oussama <oussama@notifyai.io>
2025-01-30 12:10:52 +0400

    stop opening server in test mode

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
@@ -5,16 +6,16 @@ import router from "./routes.js";
 dotenv.config();
 
 const app = express();
-// eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/", router);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
+if (process.env.NODE_ENV !== "test") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 export default app;
